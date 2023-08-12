733e504b5be356e0f1b397e1fd7b0522177cf6b1
Krishna Tiwari <kisnatwari@gmail.com>
2023-08-12 13:40:58 +0545

    setting up prisma database and docker setup for backend postgresql database

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+const prisma = new PrismaClient();
+
+export const GET = async (req: NextRequest, res: NextResponse) => {
+    try{
+        const categories = await prisma.category.findMany();
+        console.log(categories);
+        return new NextResponse(JSON.stringify(categories), {status: 200});
+    }catch(err){
+        console.log(err);
+        return new NextResponse(JSON.stringify({ message: "Something went wrong" }), {status:500});
+    }
+}
